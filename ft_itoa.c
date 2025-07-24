9993a21783f15b53bc1b6ef380e6fd90fd788040
Eddie - 42 Account <prenumeda8@gmail.com>
2025-07-24 13:33:56 +0200

    Added last two mandatory functions.
    Added the last two mandatory functions. Must test them at home & add header + edit the make file and header file :D

---
+++ ft_itoa.c
@@ -0,0 +1,51 @@
+#include "libft.h"
+
+static int	count_len(int n)
+{
+	int	len;
+
+	len = (n <= 0);
+	while (n)
+	{
+		len++;
+		n /= 10;
+	}
+	return (len);
+}
+
+static char	*fill_str(char *str, int n, int len)
+{
+	str[len] = '\0';
+	if (n == 0)
+		str[0] = '0';
+	if (n < 0)
+	{
+		str[0] = '-';
+		while (--len > 0)
+		{
+			str[len] = '0' - (n % 10);
+			n /= 10;
+		}
+	}
+	else
+	{
+		while (--len >= 0)
+		{
+			str[len] = '0' + (n % 10);
+			n /= 10;
+		}
+	}
+	return (str);
+}
+
+char	*ft_itoa(int n)
+{
+	int		len;
+	char	*str;
+
+	len = count_len(n);
+	str = malloc(len + 1);
+	if (!str)
+		return (NULL);
+	return (fill_str(str, n, len));
+}
